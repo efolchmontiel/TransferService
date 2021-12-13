8cdea7d16c3401169bec8227250b19b967f1e593
efolchmontiel <e.folchm@gmail.com>
2021-12-13 02:08:46 -0300

    Integracion

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 const {mongoose} = require('./database');
@@ -11,6 +12,7 @@ app.set('port', process.env.PORT || 3000 );
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use('/', express.static(path.join(__dirname, '/app/')));
 app.use(cors({origin: 'http://localhost:4200'}));
 // Routes
 app.use(require('./routes/banco.routes'));
